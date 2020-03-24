b7f2d68d7e5a89f9474021778f71023880b63c66
Guilherme Alves <guilherme.garcia@gmail.com>
2020-03-24 19:51:22 -0300

    Fix unit test

--- app/src/test/java/com/challenge/findPlaces/controller/FindPlacesControllerTest.java
+++ app/src/test/java/com/challenge/findPlaces/controller/FindPlacesControllerTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.challenge.findPlaces.service.FindPlacesService;
 
 @WebMvcTest(controllers = FindPlacesController.class)
+@ActiveProfiles("test")
 public class FindPlacesControllerTest {
 
 	@Autowired
